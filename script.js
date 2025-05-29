38caa1908d84289dba25ddee4148a7c1ade68fe9
henry-phippsoe <26hsoeiro@cpsd.us>
2025-05-29 13:04:36 -0400

    repeat and reverse

---
+++ script.js
@@ -0,0 +1,38 @@
+function repeatString(str, num) {
+    let endStr = '';
+    for (let i = 0; i < num; i++) {
+        endStr += str;
+    }
+    return endStr;
+};
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+};
+
+function removeFromArray() {
+
+};
+
+function sumAll() {
+
+};
+
+function leapYears() {
+
+};
+
+function convertToCelsius() {
+};
+
+function convertToFahrenheit() {
+};
+
+console.log(repeatString("hello", 5));
+console.log(reverseString("hello"));
+removeFromArray();
+sumAll();
+leapYears();
+convertToCelsius();
+convertToFahrenheit();
+
